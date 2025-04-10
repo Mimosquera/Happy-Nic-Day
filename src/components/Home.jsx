c2a3e9668b58cd9cdf6db817fd486555f9756810
Michael Mosquera <michael2000ny@gmail.com>
2025-04-10 18:33:55 -0400

    home-heart, and cakePage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,12 @@ const Home = () => {
       <Link to="/cake"><button>Blow Out the Cake</button></Link>
       <Link to="/would-you-still-date"><button>Would You Still Date Me If…</button></Link>
       <Link to="/compliments"><button>Compliment Machine</button></Link>
+      <img 
+        src="/purple-heart-pulse.gif"
+        alt="purple-heart-pulse"
+        width={250}
+        height={250}
+      />
     </div>
   );
 };
